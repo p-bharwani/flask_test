8e79b93d742613bd6b56628aeb7bb6db749db954
Pallavi Bharwani <p.bharwani@CIMSOLUTIONS.local>
2017-04-06 09:16:03 +0200

    cosmetic change

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'bby'}
+    return render_template('index.html', title = 'Home', user = user)
+
+# made using https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
