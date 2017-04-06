6bf2080771c2b9fede5ee7a8ca78131ff1226c44
Pallavi Bharwani <p.bharwani@CIMSOLUTIONS.local>
2017-04-06 14:09:12 +0200

    added code to display text in lists

--- app/views.py
+++ app/views.py
@@ -5,6 +5,16 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'bby'}
-    return render_template('index.html', title = 'Home', user = user)
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in the Netherlands!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'Beauty and the Best was so awesome!'
+        }
+    ]
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
 
 # made using https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
